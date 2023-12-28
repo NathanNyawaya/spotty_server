90abc7a281a9078925f7628e2f7cb6aa2acc3bf2
NathanNyawaya <nyawayanathan@gmail.com>
2023-12-28 21:26:06 +0300

    live updates

--- controllers/predictions/predictionsController.js
+++ controllers/predictions/predictionsController.js
@@ -28,6 +28,24 @@ export const getAllTips = async (req, res) => {
 };
 
 
+
+export const getTipOffer = async (req, res) => {
+  try {
+    const offer_id = req.params.id;
+   
+    const offer_data = await BetTip.findById(offer_id);
+    if (offer_data) {
+      console.log(`offer with id ${offer_id} fetched`);
+      res.status(200).json({ message: "Offer Document Found", offer_data });
+    } else {
+      res.status(404).json({ message: "No Offer Document Found" });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+
 export const addTip = async (req, res) => {
   try {
     // Extract data from the request body
@@ -53,3 +71,56 @@ export const addTip = async (req, res) => {
   }
 };
 
+
+
+
+// Function to edit a BetTip
+export const editTip = async (req, res) => {
+  try {
+    const offer_id = req.params.id;
+    const { betOfferName, events, passcode, status, price } = req.body;
+
+    // Find the BetTip by ID and update its fields
+    const updatedTip = await BetTip.findByIdAndUpdate(
+      offer_id,
+      {
+        betOfferName,
+        events,
+        passcode,
+        status,
+        price
+      },
+      { new: true }
+    );
+
+    if (updatedTip) {
+      console.log(`BetTip with id ${offer_id} updated`);
+      res.status(200).json({ message: "BetTip Updated", updatedTip });
+    } else {
+      res.status(404).json({ message: "No BetTip Found for Update" });
+    }
+  } catch (err) {
+    console.error('Error updating BetTip:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+// Function to delete a BetTip
+export const deleteTip = async (req, res) => {
+  try {
+    const offer_id = req.params.id;
+
+    // Find the BetTip by ID and remove it
+    const deletedTip = await BetTip.findByIdAndRemove(offer_id);
+
+    if (deletedTip) {
+      console.log(`BetTip with id ${offer_id} deleted`);
+      res.status(200).json({ message: "BetTip Deleted", deletedTip });
+    } else {
+      res.status(404).json({ message: "No BetTip Found for Deletion" });
+    }
+  } catch (err) {
+    console.error('Error deleting BetTip:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
