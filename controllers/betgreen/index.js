b3df1def394dfb101d3b4c0bbd16d33764577f68
NathanNyawaya <73029303+NathanNyawaya@users.noreply.github.com>
2024-03-03 13:31:59 +0300

    Update index.js
    refresh data manual process

--- controllers/betgreen/index.js
+++ controllers/betgreen/index.js
@@ -54,9 +54,9 @@ export const soccerLeagueService = async () => {
 export const startMaintainer = async () => {
     try {
         console.log('Running the daily maintenance job');
-        // await soccerLeagueService();
-        // await marketsService();
-        // await processLeagueEvents();
+        await soccerLeagueService();
+        await marketsService();
+        await processLeagueEvents();
         await eplMaintainer();
 
         // Schedule the daily maintenance job to run every day at midnight
